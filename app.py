d0cd36a88502f4634ef9763ebaa3256bfc341b37
Tejaswini Sudhakar <tejaswanisudhakar@gmail.com>
2022-05-26 22:56:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import Home
+import Visualization
+import Dashboard
+import Query
+from PIL import Image
+
+st.set_page_config(layout="wide")
+s = st.button("Home")
+PAGES = {
+    "Home": Home,
+    "Visualizer": Visualization,
+    "Dashboard": Dashboard,
+    "Query Solver": Query
+}
+
+st.image("Learn Leadspace 4_0.png")
+if s == True:
+    selection = st.selectbox("Choose one", list(PAGES.keys()), key ='2')
+    Home.app()
+else:
+    selection = st.selectbox("Choose one", list(PAGES.keys()), key ='3')
+    page = PAGES[selection]
+    page.app()
